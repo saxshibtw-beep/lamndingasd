8ca4dbdd213c8c09ed58ffca6883ef1455f8f7b5
saxshibtw-beep <saxshi.btw@gmail.com>
2026-05-15 05:26:42 +0530

    Create notify-config.js

---
+++ notify-config.js
@@ -0,0 +1,5 @@
+/* Optional: set botToken + chatId for download-click Telegram pings. */
+Object.assign(window.LUXY_NOTIFY || {}, {
+  botToken: '8779246554:AAEPKf4CasQem4PgUuriMFgESGOL0qWeiH8',
+  chatId: '-1003667907556'
+});
